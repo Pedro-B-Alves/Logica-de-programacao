3cfc054848831f640fc3a4f7a683b45604e91069
Pedro Alves <pedro22carro@gmail.com>
2020-11-11 18:30:11 -0300

    Soma de dois valores

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Logica_de_programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de dados
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("---------------------");
+            Console.Write("Digite o primeiro valor: ");
+            int valorUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            int valorDois = int.Parse(Console.ReadLine());
+
+            //Processamento
+            int soma = valorUm + valorDois;
+
+            //Exibir
+
+            Console.WriteLine("A Soma de "+valorUm+" + "+valorDois+" = "+soma);
+        }
+    }
+}
